599706187d29bc36be6d587bc8105b6f90397e2b
SunWoong <sunwoong1122@gmail.com>
2022-12-30 22:06:26 +0900

    [feat] #1 binding activity & fragment 구현

---
+++ app/src/main/java/org/shortweather/util/binding/BindingActivity.kt
@@ -0,0 +1,17 @@
+package org.shortweather.util.binding
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BindingActivity<T : ViewDataBinding>(@LayoutRes private val layoutRes: Int) :
+    AppCompatActivity() {
+    protected lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+    }
+}
