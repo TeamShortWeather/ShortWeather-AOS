a03d26e628641202f44b052c9c462811144b6371
SunWoong <sunwoong1122@gmail.com>
2022-12-30 22:08:05 +0900

    [feat] #1 event & event observer 구현

---
+++ app/src/main/java/org/shortweather/util/Event.kt
@@ -0,0 +1,14 @@
+package org.shortweather.util
+
+class Event<T>(private val content: T) {
+    private var hasBeenHandled = false
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+}
---
+++ app/src/main/java/org/shortweather/util/EventObserver.kt
@@ -0,0 +1,11 @@
+package org.shortweather.util
+
+import androidx.lifecycle.Observer
+
+class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
+    override fun onChanged(event: Event<T>?) {
+        event?.getContentIfNotHandled()?.let {
+            onEventUnhandledContent(it)
+        }
+    }
+}
