8311da616e6080b8cf1ff121e05cfd784d1f5cbd
SunWoong <sunwoong1122@gmail.com>
2023-01-03 02:35:46 +0900

    [feat] #4 drawer UI open 기능 구현

--- app/src/main/java/org/shortweather/presentation/MainActivity.kt
+++ app/src/main/java/org/shortweather/presentation/MainActivity.kt
@@ -1,9 +1,36 @@
 package org.shortweather.presentation
 
+import android.os.Bundle
+import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 import org.shortweather.R
 import org.shortweather.databinding.ActivityMainBinding
+import org.shortweather.presentation.todayweather.TodayWeatherFragment
 import org.shortweather.util.binding.BindingActivity
 
 @AndroidEntryPoint
-class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main)
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initView()
+        setNavigation()
+        startTargetFragment(TodayWeatherFragment())
+    }
+
+    private fun initView() {
+        binding.nvMain.itemIconTintList = null
+    }
+
+    private fun setNavigation() {
+        binding.tbMain.setNavigationOnClickListener {
+            binding.dlMain.open()
+        }
+    }
+
+    private fun startTargetFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_main, fragment)
+            .commitAllowingStateLoss()
+    }
+}
