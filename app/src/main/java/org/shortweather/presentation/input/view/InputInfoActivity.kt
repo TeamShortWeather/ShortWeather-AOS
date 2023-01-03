314122b4a8b9a0bc57e6a1f92e8658414fbe6891
CHAE <chaso1495@naver.com>
2023-01-03 17:57:27 +0900

    [ui] #3 1페이지 바텀시트 구현 완료

--- app/src/main/java/org/shortweather/presentation/input/view/InputInfoActivity.kt
+++ app/src/main/java/org/shortweather/presentation/input/view/InputInfoActivity.kt
@@ -17,9 +17,26 @@ class InputInfoActivity: BindingActivity<ActivityInputInfoBinding>(R.layout.acti
 
     private fun setOnClickListener() {
         // 다음 버튼을 누르면 InputTimeActivity로 이동
-        binding.btnInputInfoNext.setOnClickListener(){
+        binding.btnInputInfoNext.setOnClickListener(){ // 다음 버튼 클릭 시
             startActivity(Intent(this, InputTimeActivity::class.java))
             finish()
+        }
+        binding.layoutGender.setOnClickListener(){ // 성별 선택란 클릭 시
+/*            val list = mutableListOf(
+                BottomSheetItem("여자"),
+                BottomSheetItem("남자"),
+            )
+            val adapter = BottomSheetAdapter()
+            adapter.setItem(list)
+            val modalBottomSheet = BottomSheetGender(adapter)
+            val modalBottomSheet = BottomSheetGender()
+            modalBottomSheet.show(supportFragmentManager, BottomSheetGender.TAG) */
+        }
+        binding.layoutAge.setOnClickListener(){ // 나이 선택란 클릭 시
+
+        }
+        binding.layoutSense.setOnClickListener(){ // 민감도 선택란 클릭 시
+
         }
     }
 }
