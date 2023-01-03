a7a8a4ad06d5071aa6c6f0050c57f8566b3f1cc4
SunWoong <sunwoong1122@gmail.com>
2023-01-03 20:57:06 +0900

    [chore] #4 충돌 해결

---
+++ app/src/main/java/org/shortweather/presentation/splash/SplashActivity.kt
@@ -0,0 +1,32 @@
+package org.shortweather.presentation.splash
+
+import android.content.Intent
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import org.shortweather.R
+import android.os.Handler
+import android.os.Looper
+import org.shortweather.databinding.ActivitySplashBinding
+import org.shortweather.presentation.MainActivity
+import org.shortweather.presentation.input.view.InputInfoActivity
+import org.shortweather.presentation.input.view.InputTimeActivity
+import org.shortweather.util.binding.BindingActivity
+
+@AndroidEntryPoint
+class SplashActivity: BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+    private val SPLASH_TIME: Long = 3000  //3 sec
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            //main activity 시작
+            startActivity(Intent(this, InputInfoActivity::class.java))
+
+            //액티비티 닫기
+            finish()
+        }, SPLASH_TIME)
+    }
+}
