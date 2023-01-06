483bac78884962c5512698ef1978e55adbd7cb93
SunWoong <sunwoong1122@gmail.com>
2023-01-06 15:22:03 +0900

    [feat] #11 맞춤 날씨 fragment 추가

---
+++ app/src/main/java/org/shortweather/presentation/customweather/view/CustomWeatherFragment.kt
@@ -0,0 +1,14 @@
+package org.shortweather.presentation.customweather.view
+
+import android.os.Bundle
+import android.view.View
+import org.shortweather.R
+import org.shortweather.databinding.FragmentCustomWeatherBinding
+import org.shortweather.util.binding.BindingFragment
+
+class CustomWeatherFragment :
+    BindingFragment<FragmentCustomWeatherBinding>(R.layout.fragment_custom_weather) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
