e55f51eb0d9a1d157719e4185fd34ca03fbeaccf
ahra1221 <lizze011221@gmail.com>
2023-01-06 21:12:21 +0900

    [fix] #13 set adapter 함수화

--- app/src/main/java/org/shortweather/presentation/weeklyweather/view/WeeklyWeatherFragment.kt
+++ app/src/main/java/org/shortweather/presentation/weeklyweather/view/WeeklyWeatherFragment.kt
@@ -9,7 +9,6 @@ import org.shortweather.presentation.weeklyweather.adapter.WeeklyWeatherAdapter
 import org.shortweather.presentation.weeklyweather.viewmodel.WeeklyWeatherViewModel
 import org.shortweather.util.binding.BindingFragment
 
-
 class WeeklyWeatherFragment :
     BindingFragment<FragmentWeeklyWeatherBinding>(R.layout.fragment_weekly_weather) {
     private val viewModel by viewModels<WeeklyWeatherViewModel>()
@@ -17,8 +16,12 @@ class WeeklyWeatherFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = WeeklyWeatherAdapter(requireContext())
-        adapter.setWeatherList(viewModel.mockWeeklyWeatherList)
+        setAdapter()
+    }
+
+    private fun setAdapter() {
+        val adapter = WeeklyWeatherAdapter()
+        adapter.setWeatherList(viewModel.getMockWeeklyWeatherList())
         binding.rvWeeklyWeather.adapter = adapter
     }
 }
