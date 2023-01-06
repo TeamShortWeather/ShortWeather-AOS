9d086c9b35d78cfe451a1bd986398549e2b454af
ahra1221 <lizze011221@gmail.com>
2023-01-06 17:52:49 +0900

    [chore] #13 변수명 수정

--- app/src/main/java/org/shortweather/data/source/remote/ResponseWeeklyWeather.kt
+++ app/src/main/java/org/shortweather/data/source/remote/ResponseWeeklyWeather.kt
@@ -4,12 +4,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ResponseWeeklyWeather(
-    val weekly_weather_calender_week: String, // 요일
-    val weekly_weather_calendar_day: String, // 날짜
+    val weeklyWeatherCalenderWeek: String, // 요일
+    val weeklyWeatherCalendarDay: String, // 날짜
     //val weekly_weather_day_pic: String, //오전 날씨 사진
-    val weekly_weather_day_rain: String, //오전 날씨 강수량
+    val weeklyWeatherDayRain: String, //오전 날씨 강수량
     //val weekly_weather_night_pic:String, //오후 날씨 사진
-    val weekly_weather_night_rain:String, //오후 날씨 강수량
-    val weekly_weather_temp_up: String, //최고 기온
-    val weekly_weather_temp_down: String // 최저 기온
+    val weeklyWeatherNightRain:String, //오후 날씨 강수량
+    val weeklyWeatherTempUp: String, //최고 기온
+    val weeklyWeatherTempDown: String // 최저 기온
 )
