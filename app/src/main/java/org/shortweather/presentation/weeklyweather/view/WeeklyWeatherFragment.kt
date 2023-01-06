b6630dfb0a4ad92c643b8cefbc47ee565d67f575
ahra1221 <lizze011221@gmail.com>
2023-01-06 17:28:00 +0900

    [feat] #13 주간 날씨 화면 뷰모델과 어댑터 연결

---
+++ app/src/main/java/org/shortweather/presentation/weeklyweather/view/WeeklyWeatherFragment.kt
@@ -0,0 +1,24 @@
+package org.shortweather.presentation.weeklyweather.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import org.shortweather.R
+import org.shortweather.databinding.FragmentWeeklyWeatherBinding
+import org.shortweather.presentation.weeklyweather.adapter.WeeklyWeatherAdapter
+import org.shortweather.presentation.weeklyweather.viewmodel.WeeklyWeatherViewModel
+import org.shortweather.util.binding.BindingFragment
+
+
+class WeeklyWeatherFragment :
+    BindingFragment<FragmentWeeklyWeatherBinding>(R.layout.fragment_weekly_weather) {
+    private val viewModel by viewModels<WeeklyWeatherViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = WeeklyWeatherAdapter(requireContext())
+        adapter.setWeatherList(viewModel.mockWeeklyWeatherList)
+        binding.rvWeeklyWeather.adapter = adapter
+    }
+}
