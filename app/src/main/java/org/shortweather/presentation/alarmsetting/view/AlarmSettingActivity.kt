67d9ce5cf4bce35f81e926827d625534469e9c16
ahra1221 <lizze011221@gmail.com>
2023-01-07 22:59:50 +0900

    [feat] #32 알림 설정 뷰 스위치 on/off 기능 구현

--- app/src/main/java/org/shortweather/presentation/alarmsetting/view/AlarmSettingActivity.kt
+++ app/src/main/java/org/shortweather/presentation/alarmsetting/view/AlarmSettingActivity.kt
@@ -1,6 +1,7 @@
 package org.shortweather.presentation.alarmsetting.view
 
 import android.os.Bundle
+import androidx.core.view.isVisible
 import org.shortweather.R
 import org.shortweather.databinding.ActivityAlarmSettingBinding
 import org.shortweather.util.binding.BindingActivity
@@ -10,5 +11,20 @@ class AlarmSettingActivity :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.lifecycleOwner = this
+
+        binding.btnAlarmSettingAll.setOnCheckedChangeListener { view, isChecked ->
+            if (isChecked) {
+                binding.layoutAlarmSettingWake.isVisible = true
+                binding.layoutAlarmSettingSleep.isVisible = true
+                binding.layoutAlarmSettingNews.isVisible = true
+            } else {
+                binding.layoutAlarmSettingWake.isVisible = false
+                binding.layoutAlarmSettingSleep.isVisible = false
+                binding.layoutAlarmSettingNews.isVisible = false
+            }
+        }
     }
 }
+
