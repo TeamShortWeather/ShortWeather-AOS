1333b4fe19b4c2091b5a1c03fe706500a804a2be
ahra1221 <lizze011221@gmail.com>
2023-01-09 01:20:56 +0900

    [feat] #43 설정 화면 메뉴와 각 activity 연결

--- app/src/main/java/org/shortweather/presentation/setting/view/SettingFragment.kt
+++ app/src/main/java/org/shortweather/presentation/setting/view/SettingFragment.kt
@@ -1,9 +1,11 @@
 package org.shortweather.presentation.setting.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import org.shortweather.R
 import org.shortweather.databinding.FragmentSettingBinding
+import org.shortweather.presentation.alarmsetting.view.AlarmSettingActivity
 import org.shortweather.util.binding.BindingFragment
 
 class SettingFragment :
@@ -11,6 +13,23 @@ class SettingFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setOnClickListener()
+    }
+
+    fun setOnClickListener() {
+        binding.ivNextBtn1.setOnClickListener {
+            val intent = Intent(context, OtherTimeSettingActivity::class.java)
+            startActivity(intent)
+        }
+        binding.ivNextBtn2.setOnClickListener {
+            val intent = Intent(context, AlarmSettingActivity::class.java)
+            startActivity(intent)
+        }
+        binding.ivNextBtn3.setOnClickListener {
+            val intent = Intent(context, WakeTimeSettingActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
