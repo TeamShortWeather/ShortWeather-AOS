c68e243e9c7f8bf9b22575834df691944afcb929
SunWoong <sunwoong1122@gmail.com>
2023-01-09 21:09:14 +0900

    [refactor] #42 refresh 초기화 로직 함수화

--- app/src/main/java/org/shortweather/presentation/todayweather/view/TodayWeatherFragment.kt
+++ app/src/main/java/org/shortweather/presentation/todayweather/view/TodayWeatherFragment.kt
@@ -1,15 +1,16 @@
 package org.shortweather.presentation.todayweather.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import org.shortweather.R
 import org.shortweather.databinding.FragmentTodayWeatherBinding
 import org.shortweather.presentation.todayweather.adapter.TodayWeatherAdapter
 import org.shortweather.presentation.todayweather.viewmodel.TodayWeatherViewModel
 import org.shortweather.util.binding.BindingFragment
 
+@AndroidEntryPoint
 class TodayWeatherFragment :
     BindingFragment<FragmentTodayWeatherBinding>(R.layout.fragment_today_weather) {
     private val viewModel: TodayWeatherViewModel by viewModels()
@@ -17,7 +18,11 @@ class TodayWeatherFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setOnRefreshListener()
         setAdapter()
+    }
+
+    private fun setOnRefreshListener() {
         binding.srlTodayWeather.setOnRefreshListener {
             binding.srlTodayWeather.isRefreshing = false
         }
