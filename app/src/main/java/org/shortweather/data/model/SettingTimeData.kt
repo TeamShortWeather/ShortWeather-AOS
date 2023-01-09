c64a931c9ff425bd73e0c15fd44772c2ca8ec764
CHAE <chaso1495@naver.com>
2023-01-10 03:56:03 +0900

    [fix] #49 스플래쉬 화면 파일 교체

--- app/src/main/java/org/shortweather/data/model/SettingTimeData.kt
+++ app/src/main/java/org/shortweather/data/model/SettingTimeData.kt
@@ -1,7 +1,5 @@
 package org.shortweather.data.model
 
-// 1/10 00:45 -> 기상시간, 외출/귀가시간 수정 기능 구현 잠정적 연기, 추후 활용 예정
-
 data class RequestOtherTime(
     val goOutTime: String,
     val goHomeTime: String
