e484eba8ba8bb1bce5dcd9f64332b90286f31a13
SunWoong <sunwoong1122@gmail.com>
2023-01-11 00:59:07 +0900

    [feat] #51 text & background & color 커스텀 binding adapter 구현

--- app/src/main/java/org/shortweather/util/binding/BindingAdapter.kt
+++ app/src/main/java/org/shortweather/util/binding/BindingAdapter.kt
@@ -2,9 +2,11 @@ package org.shortweather.util.binding
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import coil.load
+import org.shortweather.R
 
 @BindingAdapter("imageUrl")
 fun ImageView.setImageUrl(imageUrl: String) {
@@ -15,4 +17,39 @@ fun ImageView.setImageUrl(imageUrl: String) {
 fun View.setVisibility(isVisible: Boolean?) {
     if (isVisible == null) return
     this.isVisible = isVisible
+}
+
+@BindingAdapter("subTitle")
+fun TextView.setSubTitle(temperature: Int) {
+    this.text = "어제보다 $temperature°"
+}
+
+@BindingAdapter("temperature")
+fun TextView.setTemperature(temperature: Int) {
+    this.text = "$temperature°"
+}
+
+@BindingAdapter("toast")
+fun TextView.setToast(temperature: Int) {
+    this.text = "어제는 $temperature°로"
+}
+
+@BindingAdapter("dustBackground")
+fun TextView.setDustBackground(level: Int) {
+    when (level) {
+        1 -> this.setBackgroundResource(R.drawable.bg_good_dust)
+        2 -> this.setBackgroundResource(R.drawable.bg_nomal_dust)
+        3 -> this.setBackgroundResource(R.drawable.bg_bad_dust)
+        4 -> this.setBackgroundResource(R.drawable.bg_worst_dust)
+    }
+}
+
+@BindingAdapter("dustTextColor")
+fun TextView.setDustTextColor(level: Int) {
+    when (level) {
+        1 -> this.setTextColor(context.getColor(R.color.short_weather_dustgood2))
+        2 -> this.setTextColor(context.getColor(R.color.short_weather_dustnormal2))
+        3 -> this.setTextColor(context.getColor(R.color.short_weather_dustbad2))
+        4 -> this.setTextColor(context.getColor(R.color.short_weather_dustworst2))
+    }
 }
