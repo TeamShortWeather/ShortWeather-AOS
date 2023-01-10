82e4719839e876d88ed127dfbe1bec881c3c773c
SunWoong <sunwoong1122@gmail.com>
2023-01-11 01:06:40 +0900

    [feat] #51 오늘 날씨 조회 & 오늘 날씨 toast 조회 API -> 데이터 관리 로직 구현

--- app/src/main/java/org/shortweather/presentation/todayweather/viewmodel/TodayWeatherViewModel.kt
+++ app/src/main/java/org/shortweather/presentation/todayweather/viewmodel/TodayWeatherViewModel.kt
@@ -1,22 +1,64 @@
 package org.shortweather.presentation.todayweather.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import org.shortweather.data.model.FineDust
-import org.shortweather.data.model.Newsflash
-import org.shortweather.data.model.TodayWeatherTag
-import org.shortweather.data.model.UltrafineDust
+import kotlinx.coroutines.launch
+import org.shortweather.data.model.*
+import org.shortweather.domain.repository.TodayWeatherRepository
 import javax.inject.Inject
 
 @HiltViewModel
-class TodayWeatherViewModel @Inject constructor() : ViewModel() {
-    private val tags = listOf(Newsflash("폭염특보"), FineDust(""), UltrafineDust(""))
+class TodayWeatherViewModel @Inject constructor(
+    private val todayWeatherRepository: TodayWeatherRepository
+) : ViewModel() {
+    private val _tags = MutableLiveData<List<TodayWeatherTag>>()
+    val tags: LiveData<List<TodayWeatherTag>>
+        get() = _tags
     val isSelect = MutableLiveData(false)
-
-    fun getTags(): List<TodayWeatherTag> = tags
+    private val _todayWeatherInfo = MutableLiveData<ResponseTodayWeatherInfo>()
+    val todayWeatherInfo: LiveData<ResponseTodayWeatherInfo>
+        get() = _todayWeatherInfo
+    private val _todayWeatherToastInfo = MutableLiveData<ResponseTodayWeatherToastInfo>()
+    val todayWeatherToastInfo: LiveData<ResponseTodayWeatherToastInfo>
+        get() = _todayWeatherToastInfo
 
     fun setToastEvent(isSelect: Boolean) {
         this.isSelect.value = isSelect
+    }
+
+    fun getTodayWeatherInfo(accessToken: String) {
+        viewModelScope.launch {
+            runCatching {
+                todayWeatherRepository.getTodayWeatherInfo(accessToken)
+            }.fold({
+                val todayWeatherInfo = it.data!!
+                _todayWeatherInfo.value = todayWeatherInfo
+                val tags = mutableListOf<TodayWeatherTag>()
+                if (todayWeatherInfo.breakingNews != null) {
+                    tags.add(Newsflash(todayWeatherInfo.breakingNews))
+                }
+                tags.add(FineDust(todayWeatherInfo.fineDust))
+                tags.add(UltrafineDust(todayWeatherInfo.ultrafineDust))
+                _tags.value = tags
+            }, {
+                // error 처리 필요
+            })
+        }
+    }
+
+    fun getTodayWeatherToastInfo(accessToken: String) {
+        viewModelScope.launch {
+            runCatching {
+                todayWeatherRepository.getTodayWeatherToastInfo(accessToken)
+            }.fold({
+                _todayWeatherToastInfo.value = it.data!!
+                isSelect.value = true
+            }, {
+                // error 처리 필요
+            })
+        }
     }
 }
