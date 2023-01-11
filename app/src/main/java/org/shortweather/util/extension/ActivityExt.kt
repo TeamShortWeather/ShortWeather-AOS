55e9383ae97c432bf9e13a44f7a2c9c11fb284ac
SunWoong <sunwoong1122@gmail.com>
2023-01-12 03:05:32 +0900

    [feat] #70 activity -> fragment replace 확장함수 추가

--- app/src/main/java/org/shortweather/util/extension/ActivityExt.kt
+++ app/src/main/java/org/shortweather/util/extension/ActivityExt.kt
@@ -1,7 +1,18 @@
 package org.shortweather.util.extension
 
 import android.view.View
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+
+inline fun <reified T : Fragment> AppCompatActivity.replace(@IdRes frameId: Int) {
+    supportFragmentManager.commit {
+        replace<T>(frameId)
+        setReorderingAllowed(true)
+    }
+}
 
 fun AppCompatActivity.hideKeyboard() {
     hideKeyboard(currentFocus ?: View(this))
