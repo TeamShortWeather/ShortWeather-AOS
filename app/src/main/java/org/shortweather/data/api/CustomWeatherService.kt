141f62461398d081259788854472a65c67517837
ahra1221 <lizze011221@gmail.com>
2023-01-12 04:52:18 +0900

    [feat] #76 맞춤 날씨 화면 service 구현

--- app/src/main/java/org/shortweather/data/api/CustomWeatherService.kt
+++ app/src/main/java/org/shortweather/data/api/CustomWeatherService.kt
@@ -5,23 +5,26 @@ import org.shortweather.data.model.ResponseCustomWeatherDetail
 import org.shortweather.data.model.ResponseCustomWeatherRain
 import org.shortweather.data.model.ResponseCustomWeatherTemp
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface CustomWeatherService {
-//    @GET("/weather/today/detail")
-//    suspend fun getDetail(
-//    ): BaseResponse <ResponseCustomWeatherDetail>
+    /**
+     * 오늘 날씨 정보 조회 API
+     */
+    @GET("/weather/today/detail")
+    suspend fun getDetail(
+        @Header("Authorization") accessToken: String
+    ): BaseResponse<ResponseCustomWeatherDetail>
 
     /**
      * 시간대별 날씨 화면 날씨 조회 API
      */
     @GET("weather/today/detail/temp")
-    suspend fun getTemp():
-            BaseResponse<List<ResponseCustomWeatherTemp>>
+    suspend fun getTemp(): BaseResponse<List<ResponseCustomWeatherTemp>>
 
     /**
      * 시간대별 날씨 화면 강수 조회 API
      */
     @GET("weather/today/detail/rain")
-    suspend fun getRain():
-            BaseResponse<List<ResponseCustomWeatherRain>>
+    suspend fun getRain(): BaseResponse<List<ResponseCustomWeatherRain>>
 }
