c6aa67d70ab95d9fe3a87b9eb135eaa1464a5f80
ahra1221 <lizze011221@gmail.com>
2023-01-12 00:34:34 +0900

    [feat] #53 시간대별 날씨 조회 뷰모델 구현

--- app/src/main/java/org/shortweather/presentation/customweather/viewmodel/CustomWeatherViewModel.kt
+++ app/src/main/java/org/shortweather/presentation/customweather/viewmodel/CustomWeatherViewModel.kt
@@ -1,33 +1,31 @@
 package org.shortweather.presentation.customweather.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import org.shortweather.data.model.CustomWeatherPrecipitation
-import org.shortweather.data.model.CustomWeatherWeather
+import org.shortweather.data.model.CustomWeatherTemp
+import org.shortweather.domain.repository.CustomWeatherRepository
 import org.shortweather.util.Constants.WEATHER
 import javax.inject.Inject
 
 @HiltViewModel
-class CustomWeatherViewModel @Inject constructor() : ViewModel() {
+class CustomWeatherViewModel @Inject constructor(
+    private val customWeatherRepository: CustomWeatherRepository
+) : ViewModel() {
+    private val _customWeatherTempList = MutableLiveData<List<CustomWeatherTemp>>()
+    val customWeatherTempList: LiveData<List<CustomWeatherTemp>>
+        get() = _customWeatherTempList
+
+    private val _customWeatherRainList = MutableLiveData<List<CustomWeatherPrecipitation>>()
+    val customWeatherRainList: LiveData<List<CustomWeatherPrecipitation>>
+        get() = _customWeatherRainList
+
     val isWeather = MutableLiveData(true)
     val isPrecipitation = MutableLiveData(false)
-    val weatherList = listOf(
-        CustomWeatherWeather("1", "22"),
-        CustomWeatherWeather("2", "22"),
-        CustomWeatherWeather("3", "22"),
-        CustomWeatherWeather("4", "22"),
-        CustomWeatherWeather("5", "22"),
-        CustomWeatherWeather("6", "22")
-    )
-    val precipitationList = listOf(
-        CustomWeatherPrecipitation("1", "23"),
-        CustomWeatherPrecipitation("2", "23"),
-        CustomWeatherPrecipitation("3", "23"),
-        CustomWeatherPrecipitation("4", "23"),
-        CustomWeatherPrecipitation("5", "23"),
-        CustomWeatherPrecipitation("6", "23"),
-    )
 
     fun setTimeZoneWeather(target: String) {
         if (target == WEATHER) {
@@ -38,4 +36,48 @@ class CustomWeatherViewModel @Inject constructor() : ViewModel() {
             isPrecipitation.value = true
         }
     }
+
+    fun getTemp() {
+        viewModelScope.launch {
+            runCatching {
+                customWeatherRepository.getTemp()
+            }.fold({
+//                Log.d("tag", it.data.toString())
+                val customWeatherTempList = mutableListOf<CustomWeatherTemp>()
+                val data = it.data!!
+                for (i in data.indices) {
+                    if (i == 0) {
+                        customWeatherTempList.add(CustomWeatherTemp(data[i], true))
+                    } else {
+                        customWeatherTempList.add(CustomWeatherTemp(data[i]))
+                    }
+                }
+                _customWeatherTempList.value = customWeatherTempList
+            }, {
+                //error
+            })
+        }
+    }
+
+    fun getRain() {
+        viewModelScope.launch {
+            runCatching {
+                customWeatherRepository.getRain()
+            }.fold({
+                val customWeatherRainList = mutableListOf<CustomWeatherPrecipitation>()
+                val data = it.data!!
+                for (i in data.indices) {
+                    if (i == 0) {
+                        customWeatherRainList.add(CustomWeatherPrecipitation(data[i], true))
+                    } else {
+                        customWeatherRainList.add(CustomWeatherPrecipitation(data[i]))
+                    }
+                }
+                _customWeatherRainList.value = customWeatherRainList
+            }, {
+                //error
+            })
+        }
+    }
+
 }
