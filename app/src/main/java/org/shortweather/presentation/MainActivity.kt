87f7b8698780bac6cede8741aa82e0937531c05a
SunWoong <sunwoong1122@gmail.com>
2023-01-12 03:07:25 +0900

    [feat] #70 drawer layout navigation 기능 구현

--- app/src/main/java/org/shortweather/presentation/MainActivity.kt
+++ app/src/main/java/org/shortweather/presentation/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 import org.shortweather.R
 import org.shortweather.databinding.ActivityMainBinding
+import org.shortweather.presentation.setting.view.SettingFragment
 import org.shortweather.presentation.todayweathercontainer.view.TodayWeatherContainerFragment
+import org.shortweather.presentation.weeklyweather.view.WeeklyWeatherFragment
 import org.shortweather.util.binding.BindingActivity
+import org.shortweather.util.extension.replace
 
 @AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
@@ -14,12 +17,21 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         super.onCreate(savedInstanceState)
 
         initView()
+        setNavigationItemSelectedListener()
         setNavigation()
-        startTargetFragment(TodayWeatherContainerFragment())
     }
 
     private fun initView() {
         binding.nvMain.itemIconTintList = null
+        startTargetFragment(TodayWeatherContainerFragment())
+    }
+
+    private fun setNavigationItemSelectedListener() {
+        binding.nvMain.setNavigationItemSelectedListener { menuItem ->
+            startTargetFragment(menuItem.itemId)
+            binding.dlMain.close()
+            true
+        }
     }
 
     private fun setNavigation() {
@@ -32,5 +44,13 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         supportFragmentManager.beginTransaction()
             .replace(R.id.fcv_main, fragment)
             .commitAllowingStateLoss()
+    }
+
+    private fun startTargetFragment(itemId: Int) = when (itemId) {
+        R.id.today_weather_menu_drawer -> replace<TodayWeatherContainerFragment>(R.id.fcv_main)
+        R.id.weekly_weather_menu_drawer -> replace<WeeklyWeatherFragment>(R.id.fcv_main)
+        R.id.clothes_menu_drawer -> {}
+        R.id.setting_menu_drawer -> replace<SettingFragment>(R.id.fcv_main)
+        else -> throw IllegalArgumentException("Not found error.")
     }
 }
