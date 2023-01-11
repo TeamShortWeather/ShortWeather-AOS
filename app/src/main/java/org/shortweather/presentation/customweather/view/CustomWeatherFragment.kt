c2c2b9aa8d489a7832eece8aa1b8ce5535091b52
ahra1221 <lizze011221@gmail.com>
2023-01-12 04:54:27 +0900

    [feat] #76 맞춤 날씨 화면 fragment 구현

--- app/src/main/java/org/shortweather/presentation/customweather/view/CustomWeatherFragment.kt
+++ app/src/main/java/org/shortweather/presentation/customweather/view/CustomWeatherFragment.kt
@@ -11,6 +11,7 @@ import org.shortweather.presentation.customweather.adapter.CustomWeatherTempAdap
 import org.shortweather.presentation.customweather.viewmodel.CustomWeatherViewModel
 import org.shortweather.util.Constants.FINE_DUST
 import org.shortweather.util.Constants.WEATHER
+import org.shortweather.util.ShortWeatherSharedPreference
 import org.shortweather.util.binding.BindingFragment
 
 @AndroidEntryPoint
@@ -34,7 +35,9 @@ class CustomWeatherFragment :
     private fun initView() {
         binding.rvCustomWeather.adapter = tempAdapter
         viewModel.getTemp()
+        viewModel.getDetail(ShortWeatherSharedPreference.getAccessToken(requireContext()))
     }
+
 
     private fun setOnClickListener() {
         with(binding) {
