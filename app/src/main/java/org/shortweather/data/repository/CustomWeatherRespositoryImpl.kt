7ace7819811837d4fe2cadb2c692b0083c76d7b4
ahra1221 <lizze011221@gmail.com>
2023-01-12 04:53:10 +0900

    [feat] #76 맞춤 날씨 화면 repository 구현

--- app/src/main/java/org/shortweather/data/repository/CustomWeatherRespositoryImpl.kt
+++ app/src/main/java/org/shortweather/data/repository/CustomWeatherRespositoryImpl.kt
@@ -1,6 +1,7 @@
 package org.shortweather.data.repository
 
 import org.shortweather.data.model.BaseResponse
+import org.shortweather.data.model.ResponseCustomWeatherDetail
 import org.shortweather.data.model.ResponseCustomWeatherRain
 import org.shortweather.data.model.ResponseCustomWeatherTemp
 import org.shortweather.data.source.remote.CustomWeatherDataSource
@@ -11,6 +12,9 @@ class CustomWeatherRepositoryImpl @Inject constructor(
     private val customWeatherDataSource: CustomWeatherDataSource
 ) :
     CustomWeatherRepository {
+    override suspend fun getDetail(accessToken: String): BaseResponse<ResponseCustomWeatherDetail> =
+        customWeatherDataSource.getDetail(accessToken)
+
     override suspend fun getTemp(): BaseResponse<List<ResponseCustomWeatherTemp>> =
         customWeatherDataSource.getTemp()
 
--- app/src/main/java/org/shortweather/domain/repository/CustomWeatherRepository.kt
+++ app/src/main/java/org/shortweather/domain/repository/CustomWeatherRepository.kt
@@ -1,12 +1,17 @@
 package org.shortweather.domain.repository
 
 import org.shortweather.data.model.BaseResponse
+import org.shortweather.data.model.ResponseCustomWeatherDetail
 import org.shortweather.data.model.ResponseCustomWeatherRain
 import org.shortweather.data.model.ResponseCustomWeatherTemp
 
 interface CustomWeatherRepository {
-    suspend fun getTemp() :
+    suspend fun getDetail(accessToken: String):
+            BaseResponse<ResponseCustomWeatherDetail>
+
+    suspend fun getTemp():
             BaseResponse<List<ResponseCustomWeatherTemp>>
-    suspend fun getRain() :
+
+    suspend fun getRain():
             BaseResponse<List<ResponseCustomWeatherRain>>
 }
