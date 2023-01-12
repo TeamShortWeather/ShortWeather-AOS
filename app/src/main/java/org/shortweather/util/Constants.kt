c58b912130e243e6462e9fd876992d3c5869b357
SunWoong <sunwoong1122@gmail.com>
2023-01-13 01:27:39 +0900

    [feat] #96 network exception constants 정의

--- app/src/main/java/org/shortweather/util/Constants.kt
+++ app/src/main/java/org/shortweather/util/Constants.kt
@@ -3,4 +3,10 @@ package org.shortweather.util
 object Constants {
     const val WEATHER = "weather"
     const val FINE_DUST = "fineDust"
+    const val SUCCESS_200 = 200
+    const val SUCCESS_201 = 201
+    const val HTTP_EXCEPTION_400 = 400
+    const val HTTP_EXCEPTION_401 = 401
+    const val HTTP_EXCEPTION_500 = 500
+    const val FAILURE = 0
 }
