b2392518b8a3807690a3d0bfb4a14dc0b652f8bf
ahra1221 <lizze011221@gmail.com>
2023-01-13 04:33:30 +0900

    [chore] #94 주간 날씨 화면 뷰모델 더미데이터 삽입 및 수정

--- app/src/main/java/org/shortweather/presentation/weeklyweather/viewmodel/WeeklyWeatherViewModel.kt
+++ app/src/main/java/org/shortweather/presentation/weeklyweather/viewmodel/WeeklyWeatherViewModel.kt
@@ -4,31 +4,118 @@ import androidx.lifecycle.ViewModel
 import org.shortweather.data.model.WeeklyWeather
 
 class WeeklyWeatherViewModel : ViewModel() {
-    private val mockWeeklyWeatherList = listOf<WeeklyWeather?>(
-        null,
+    private val mockWeeklyWeatherList = listOf(
+        WeeklyWeather(
+            day = "오늘",
+            date = "0114",
+            dayImage = "비 또는 눈",
+            dayRain = 60,
+            nightImage = "진눈깨비",
+            nightRain = 60,
+            isDay = true,
+            minTemp = 6,
+            maxTemp = 8
+        ),
+        WeeklyWeather(
+            day = "내일",
+            date = "0115",
+            dayImage = "진눈깨비",
+            dayRain = 60,
+            nightImage = "흐림",
+            nightRain = 30,
+            isDay = true,
+            minTemp = 0,
+            maxTemp = 3
+        ),
         WeeklyWeather(
             day = "월",
-            date = "0112",
+            date = "0116",
+            dayImage = "구름많음",
+            dayRain = 30,
+            nightImage = "맑음",
+            nightRain = 0,
+            isDay = true,
+            minTemp = -5,
+            maxTemp = 2
+        ),
+        WeeklyWeather(
+            day = "화",
+            date = "0117",
+            dayImage = "구름많음",
+            dayRain = 30,
+            nightImage = "구름많음",
+            nightRain = 30,
+            isDay = true,
+            minTemp = -7,
+            maxTemp = 1
+        ),
+        WeeklyWeather(
+            day = "수",
+            date = "0118",
             dayImage = "맑음",
             dayRain = 0,
-            nightImage= "구름많음",
-            nightRain = 70,
+            nightImage = "맑음",
+            nightRain = 0,
             isDay = true,
-            minTemp = -2,
-            maxTemp = 9
+            minTemp = -4,
+            maxTemp = 2
+        ),
+        WeeklyWeather(
+            day = "목",
+            date = "0119",
+            dayImage = "구름많음",
+            dayRain = 30,
+            nightImage = "구름많음",
+            nightRain = 30,
+            isDay = true,
+            minTemp = -5,
+            maxTemp = 3
+        ),
+        WeeklyWeather(
+            day = "금",
+            date = "0120",
+            dayImage = "맑음",
+            dayRain = 0,
+            nightImage = "맑음",
+            nightRain = 0,
+            isDay = true,
+            minTemp = -6,
+            maxTemp = -1
+        ),
+        WeeklyWeather(
+            day = "토",
+            date = "0121",
+            dayImage = "맑음",
+            dayRain = 0,
+            nightImage = "맑음",
+            nightRain = 0,
+            isDay = true,
+            minTemp = -8,
+            maxTemp = -1
         ),
         WeeklyWeather(
             day = "일",
-            date = "0112",
-            dayImage = "맑음",
-            dayRain = 50,
-            nightImage= "구름많음",
-            nightRain = 70,
+            date = "0122",
+            dayImage = "구름많음",
+            dayRain = 40,
+            nightImage = "비",
+            nightRain = 60,
             isDay = true,
-            minTemp = -2,
-            maxTemp = 9
+            minTemp = 0,
+            maxTemp = 5
+        ),
+        WeeklyWeather(
+            day = "월",
+            date = "0123",
+            dayImage = "이슬비",
+            dayRain = 50,
+            nightImage = "구름많음",
+            nightRain = 40,
+            isDay = true,
+            minTemp = 4,
+            maxTemp = 6
         )
     )
 
-    fun getMockWeeklyWeatherList(): List<WeeklyWeather?> = mockWeeklyWeatherList
+    fun getMockWeeklyWeatherList(): List<WeeklyWeather> = mockWeeklyWeatherList
 }
