e43e596f256cde53a7ca646941903b4e0ff527aa
SunWoong <sunwoong1122@gmail.com>
2023-01-13 15:22:48 +0900

    [del] #114 sample module 제거

--- app/src/main/java/org/shortweather/di/RepositoryModule.kt
+++ app/src/main/java/org/shortweather/di/RepositoryModule.kt
@@ -6,21 +6,15 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import org.shortweather.data.repository.AuthRepositoryImpl
 import org.shortweather.data.repository.CustomWeatherRepositoryImpl
-import org.shortweather.data.repository.SampleRepositoryImpl
 import org.shortweather.data.repository.TodayWeatherRepositoryImpl
 import org.shortweather.domain.repository.AuthRepository
 import org.shortweather.domain.repository.CustomWeatherRepository
-import org.shortweather.domain.repository.SampleRepository
 import org.shortweather.domain.repository.TodayWeatherRepository
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
-    @Binds
-    @Singleton
-    abstract fun bindsSampleRepository(sampleRepositoryImpl: SampleRepositoryImpl): SampleRepository
-
     @Binds
     @Singleton
     abstract fun bindsAuthRepository(authRepositoryImpl: AuthRepositoryImpl): AuthRepository
